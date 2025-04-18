c839df59d3afb1e9882f37fca57a6fd147ba70ca
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-18 18:16:55 +0600

    added pages and router files

---
+++ src/Routers/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+export const route = createBrowserRouter([
+    {
+        path: '/',
+        Component: App
+    }
+])
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
+import { route } from './Routers/Router.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={route} />
 )
